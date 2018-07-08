64d7570358dae41f43180391015faab72ec013bb
Milton444 <miltondutra2012@gmail.com>
2018-07-07 22:57:48 -0300

    Atualizar

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player{
+	private Animator animPlayer;
+	private Rigidbody2D rgbPlayer;
+	private Vector2 forceJump;
+	private Vector3 scaleInicial;
+	private float speedPlayer = 10f, horizontal, vertical;
+	private int idAnimation;
+	private Transform playerTransform;
+	public bool isGround;
+	public Player(Animator animPlayer, Rigidbody2D rgbPlayer, Transform playerTransform){
+		this.animPlayer = animPlayer;
+		this.rgbPlayer = rgbPlayer;
+		this.playerTransform = playerTransform;
+		this.forceJump = new Vector2 (0, 500f);
+		this.scaleInicial = playerTransform.localScale;
+	}
+	public void fixedUpdatePlayer(){
+		animPlayer.SetFloat ("speedY", rgbPlayer.velocity.y);
+	}
+	public void inputControllerPlayer(){
+		horizontal = Input.GetAxisRaw ("Horizontal");
+		vertical =  Input.GetAxisRaw ("Vertical");
+
+		if (horizontal > 0) {
+			idAnimation = 1;
+			playerTransform.localScale = scaleInicial;
+			rgbPlayer.velocity = new Vector2 (speedPlayer, rgbPlayer.velocity.y);
+		} else if (horizontal < 0) {
+			idAnimation = 1;
+			playerTransform.localScale = new Vector3(-1*scaleInicial.x,scaleInicial.y,scaleInicial.z);
+			rgbPlayer.velocity = new Vector2 (-speedPlayer, rgbPlayer.velocity.y);
+		} else if (horizontal == 0) {
+			rgbPlayer.velocity = new Vector2 (0, rgbPlayer.velocity.y);
+			idAnimation = 0;
+		}
+		if (Input.GetButtonDown ("Jump")&&isGround) {
+			rgbPlayer.AddForce (forceJump);
+		}
+		animPlayer.SetInteger ("idAnimation", idAnimation);
+		animPlayer.SetBool ("isGround", isGround);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public LayerMask layerGound;
+	public Transform groundCheckLeft,groundCheckRight;
+	public bool isGround;
+	private Player playerScript;
+	void Start () {
+		playerScript = new Player (GetComponent<Animator> (), GetComponent<Rigidbody2D> (), transform);
+	}
+	void FixedUpdate(){
+		playerScript.fixedUpdatePlayer ();
+	}
+	void Update () {
+		playerScript.isGround = Physics2D.OverlapCircle (groundCheckRight.position, 0.01f, layerGound) || 
+			Physics2D.OverlapCircle (groundCheckLeft.position, 0.01f, layerGound)  ;
+		playerScript.inputControllerPlayer ();
+	}
+}
